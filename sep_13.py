5e3ed516ddb4013d51345b46f9cb1f39c2d9e1e0
Kali Linux <kali@ip-172-31-82-154.ec2.internal>
2022-10-13 22:37:28 +0000

    Pushing Carly's Clippers

---
+++ sep_13.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+#Concantenation
+string1 = "The wind, "
+string2 = "which had hiterto carried us along..."
+
+message = string1 + string2
+
+#print(message)
+
+#Plus-equal
+
+num1 = 5
+#print(num1)
+
+num1 += 3
+#print(num1)
+
+#Multi-line String
+
+to_you = """ Hello,
+it's me"""
+
+print(to_you)
